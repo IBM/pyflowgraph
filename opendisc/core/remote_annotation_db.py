2671db1512df985a44b8cef9754fe19ae926340b
epatters <ejpatters@gmail.com>
2018-08-07 19:56:12 -0700

    REFACTOR: Use new REST API instead of CouchDB in remote annotation DB.

--- opendisc/core/remote_annotation_db.py
+++ opendisc/core/remote_annotation_db.py
@@ -16,7 +16,7 @@ from __future__ import absolute_import
 
 from pathlib2 import Path
 
-import couchdb
+import requests
 from traitlets import Bool, Dict, Instance, Unicode, default
 from traitlets.config import Configurable, PyFileConfigLoader
 
@@ -27,22 +27,18 @@ from .annotation_db import AnnotationDB
 class RemoteAnnotationDB(AnnotationDB, Configurable):
     """ An in-memory annotation database that pulls from a remote server.
     
-    This local database pulls the annotation documents from a remote CouchDB
-    database, but supports efficient in-memory queries to avoid repeatedly
-    hitting the remote server.
+    This local database pulls the annotation documents from a remote database,
+    but supports efficient in-memory queries to avoid repeatedly hitting the
+    remote server.
     
     The class contains no Python-specific annotation logic. For that,
     see `opendisc.kernel.trace.annotator`.
     """
     
-    # URL of CouchDB (or Cloudant) server hosting the annotations.
-    database_url = Unicode().tag(config=True)
-    
-    # Name of CouchDB (or Cloudant) database containing the annotations.
-    database_name = Unicode().tag(config=True)
+    # URL of REST API supplying the annotations.
+    api_url = Unicode().tag(config=True)
     
     # Private traits.
-    _couchdb = Instance(couchdb.Database)
     _initialized = Bool(False)
     _loaded = Dict()
     
@@ -61,19 +57,13 @@ class RemoteAnnotationDB(AnnotationDB, Configurable):
         database, then this method is a no-op (no request is made to the remote
         server). Thus, it is safe to call this method often.
         
-        Returns whether annotations were load from the server.
+        Returns whether annotations were loaded from the server.
         """
         if not self._prepare_load() or self._loaded.get((language,package), True):
             return False
         
-        query = {
-            "selector": {
-                "schema": "annotation",
-                "language": language,
-                "package": package
-            }
-        }
-        self.load_documents(self._couchdb.find(query))
+        endpoint = "/annotations/{lang}/{pkg}".format(lang=language, pkg=package)
+        self.load_documents(self._api_get(endpoint))
         self._loaded[(language,package)] = True
         return True
     
@@ -88,17 +78,20 @@ class RemoteAnnotationDB(AnnotationDB, Configurable):
         if not self._prepare_load() or self._loaded.get(language, True):
             return False
         
-        query = {
-            "selector": {
-                "schema": "annotation",
-                "language": language
-            }
-        }
-        self.load_documents(self._couchdb.find(query))
+        endpoint = "/annotations/{lang}".format(lang=language)
+        self.load_documents(self._api_get(endpoint))
         self._loaded[language] = True
         return True
     
     # Private interface
+
+    def _api_get(self, endpoint):
+        """ Make a GET request to the REST API.
+
+        Returns the JSON response data.
+        """
+        response = requests.get(self.api_url + endpoint)
+        return response.json()
     
     def _initialize(self):
         """ Initialize the annotation database by fetching the list of
@@ -107,13 +100,13 @@ class RemoteAnnotationDB(AnnotationDB, Configurable):
         Returns whether the languages and packages were fetched.
         """
         self._loaded = {}
-        if not (self.database_url and self.database_name):
+        if not self.api_url:
             return False
         
-        for row in self._couchdb.view('query/annotation_index', group=True):
-            schema, language, package = row.key
+        for language in self._api_get("/count/annotation").keys():
             self._loaded[language] = False
-            self._loaded[(language,package)] = False
+            for package in self._api_get("/count/annotation/"+language).keys():
+                self._loaded[(language,package)] = False            
         
         self._initialized = True
         return True
@@ -122,11 +115,3 @@ class RemoteAnnotationDB(AnnotationDB, Configurable):
         """ Prepare to load annotations from the remote database.
         """
         return self._initialized or self._initialize()
-    
-    # Trait initializers
-
-    @default("_couchdb")
-    def _couchdb_default(self):
-        server = couchdb.Server(self.database_url)
-        db = server[self.database_name]
-        return db
